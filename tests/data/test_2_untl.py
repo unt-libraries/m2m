04c11954baf3124cc6fb46e6d5e8d54ed39324de
Mark Phillips <vphill@gmail.com>
2020-06-10 19:06:23 -0500

    Adding test mapping file.
    This can be run with the `m2m.py` command. 
    
    Here is what I would expect to get as output from the command.
    
    ```bash
    mphillips@LSLDW370134434 test % python3 m2m/m2m/m2m.py -m test_2_untl.py test.csv
    Processing CSV file test.csv with mapping test_2_untl.py
    Processing row 0
    <?xml version="1.0" encoding="UTF-8"?>
    <metadata>
      <title qualifier="officialtitle">Graceling</title>
      <creator qualifier="aut">
        <name>Kristin Cashore</name>
        <info>born somewhere</info>
        <type>per</type>
      </creator>
      <date qualifier="creation">2008</date>
      <meta qualifier="metadataCreator">mphillips</meta>
    </metadata>
    ```

---
+++ tests/data/test_2_untl.py
@@ -0,0 +1,14 @@
+def processRecord(RecordClass, row):
+   
+    record = RecordClass('mphillips')
+
+    record.mapping('basic', 'title', row['title'],
+                    qualifier='officialtitle')
+
+    record.mapping('agent', 'creator', row['author'],
+                    qualifier='aut', agent_type='per', info='born somewhere')
+
+    record.mapping('basic', 'date', row['date'],
+                    qualifier='creation', required=False)
+
+    return record
